7ded11d0c04c74477fac68a7422c5b589d971c8b
moxiaoxue <43922687+moxiaoxue@users.noreply.github.com>
2019-09-22 20:07:21 +0800

    add

---
+++ mygithub/day01.py
@@ -0,0 +1,15 @@
+# import this
+import turtle
+# 构建一个画布
+turtle.pensize(4)
+
+# 画笔的颜色是红色的
+turtle.pencolor('red')
+# 向前200
+turtle.forward(200)
+# 左转90度
+turtle.right(90)
+# 再向前200
+turtle.forward(200)
+# 保持图像不变
+turtle.mainloop()
